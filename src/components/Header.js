228b6c2414fd1ab4243d26f470617c42b36e46cc
Shreyaapandeyy <shrey.py@gmail.com>
2024-10-12 16:24:36 +0530

    form handling

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <div className="absolute bg-gradient-to-b from-black z-40" /**bg-gradient to bottom is giving a gradient of black from top side towards bottom  */> 
+        <img className="w-36 " src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"></img>
+    </div>
+  )
+}
+
+export default Header
